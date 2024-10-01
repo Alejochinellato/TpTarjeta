556f39f962c2d2dfe80161a628ef215d3dab0395
Alejochinellato <164072406+Alejochinellato@users.noreply.github.com>
2024-10-01 16:38:51 -0300

    Saldo negativo
    Implementacion del saldo negativo

--- Tarjeta.cs
+++ Tarjeta.cs
@@ -1,3 +1,54 @@
+namespace TransporteUrbano
+{
+    public class Tarjeta
+    {
+        private decimal saldo;
+        private const decimal LimiteSaldo = 9900m;
+        private const decimal CostoPasaje = 940m;
+        private const decimal LimiteNegativo = -480m;
+        private static readonly decimal[] MontosAceptados = { 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000 };
+
+        public Tarjeta(decimal saldoInicial)
+        {
+            saldo = saldoInicial;
+        }
+
+        public bool CargarSaldo(decimal monto)
+        {
+            if (!MontosAceptados.Contains(monto))
+            {
+                return false;
+            }
+
+            if (saldo + monto > LimiteSaldo)
+            {
+                saldo = LimiteSaldo;
+            }
+            else
+            {
+                saldo += monto;
+            }
+
+            return true;
+        }
+
+        public bool DescontarPasaje()
+        {
+            if (saldo >= CostoPasaje || saldo - CostoPasaje >= LimiteNegativo)
+            {
+                saldo -= CostoPasaje;
+                return true;
+            }
+
+            return false;
+        }
+
+        public decimal ObtenerSaldo()
+        {
+            return saldo;
+        }
+    }
+}
 using System;
 using System.Linq;
 

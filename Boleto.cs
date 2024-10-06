78a3e33bfc5f71c83477b93916efb09225c27e6d
galleanof <110426060+galleanof@users.noreply.github.com>
2024-10-06 15:52:56 -0300

    Update Boleto.cs

--- Boleto.cs
+++ Boleto.cs
@@ -4,18 +4,24 @@ namespace TransporteUrbano
 {
     public class Boleto
     {
-        private string linea;
-        private decimal costo;
+        public decimal Monto { get; }
+        public string Tipo { get; }
+        public string Linea { get; }
+        public decimal SaldoRestante { get; }
+        public int ViajeId { get; }
 
-        public Boleto(string linea, decimal costo)
+        public Boleto(decimal monto, string tipo, string linea, decimal saldoRestante, int viajeId)
         {
-            this.linea = linea;
-            this.costo = costo;
+            Monto = monto;
+            Tipo = tipo;
+            Linea = linea;
+            SaldoRestante = saldoRestante;
+            ViajeId = viajeId;
         }
 
-        public override string ToString()
+        public void MostrarDetalles()
         {
-            return $"Boleto para la línea {linea}, costo: {costo}";
+            Console.WriteLine($"Boleto {ViajeId} - Tipo: {Tipo} - Línea: {Linea} - Monto: ${Monto} - Saldo restante: ${SaldoRestante}");
         }
     }
 }

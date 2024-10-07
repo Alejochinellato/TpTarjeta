817178a37555207267aa8fcbf08b8bfe44be13f3
Alejochinellato <164072406+Alejochinellato@users.noreply.github.com>
2024-10-07 19:03:19 -0300

    Update Boleto.cs

--- Boleto.cs
+++ Boleto.cs
@@ -9,19 +9,29 @@ namespace TransporteUrbano
         public string Linea { get; }
         public decimal SaldoRestante { get; }
         public int ViajeId { get; }
+        public DateTime Fecha { get; }
+        public int TarjetaId { get; }
+        public string DescripcionSaldoNegativo { get; }
 
-        public Boleto(decimal monto, string tipo, string linea, decimal saldoRestante, int viajeId)
+        public Boleto(decimal monto, string tipo, string linea, decimal saldoRestante, int viajeId, DateTime fecha, int tarjetaId, string descripcionSaldoNegativo = "")
         {
             Monto = monto;
             Tipo = tipo;
             Linea = linea;
             SaldoRestante = saldoRestante;
             ViajeId = viajeId;
+            Fecha = fecha;
+            TarjetaId = tarjetaId;
+            DescripcionSaldoNegativo = descripcionSaldoNegativo;
         }
 
         public void MostrarDetalles()
         {
-            Console.WriteLine($"Boleto {ViajeId} - Tipo: {Tipo} - Línea: {Linea} - Monto: ${Monto} - Saldo restante: ${SaldoRestante}");
+            Console.WriteLine($"Boleto {ViajeId} - Tipo: {Tipo} - Línea: {Linea} - Monto: ${Monto} - Saldo restante: ${SaldoRestante} - Fecha: {Fecha.ToShortDateString()} - ID Tarjeta: {TarjetaId}");
+            if (!string.IsNullOrEmpty(DescripcionSaldoNegativo))
+            {
+                Console.WriteLine(DescripcionSaldoNegativo);
+            }
         }
     }
 }

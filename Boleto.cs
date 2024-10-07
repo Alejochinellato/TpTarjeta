45308e1c44a5787f351d18e0e15a28c6d486f8d7
Alejochinellato <164072406+Alejochinellato@users.noreply.github.com>
2024-10-07 19:34:08 -0300

    Update Boleto.cs

--- Boleto.cs
+++ Boleto.cs
@@ -9,19 +9,32 @@ namespace TransporteUrbano
         public string Linea { get; }
         public decimal SaldoRestante { get; }
         public int ViajeId { get; }
+        public DateTime Fecha { get; }
+        public string TipoTarjeta { get; }
+        public int TarjetaId { get; }
+        public bool SaldoNegativoCancelado { get; }
 
-        public Boleto(decimal monto, string tipo, string linea, decimal saldoRestante, int viajeId)
+        public Boleto(decimal monto, string tipo, string linea, decimal saldoRestante, int viajeId, DateTime fecha, string tipoTarjeta, int tarjetaId, bool saldoNegativoCancelado)
         {
             Monto = monto;
             Tipo = tipo;
             Linea = linea;
             SaldoRestante = saldoRestante;
             ViajeId = viajeId;
+            Fecha = fecha;
+            TipoTarjeta = tipoTarjeta;
+            TarjetaId = tarjetaId;
+            SaldoNegativoCancelado = saldoNegativoCancelado;
         }
 
         public void MostrarDetalles()
         {
-            Console.WriteLine($"Boleto {ViajeId} - Tipo: {Tipo} - Línea: {Linea} - Monto: ${Monto} - Saldo restante: ${SaldoRestante}");
+            Console.WriteLine($"Boleto {ViajeId} - Fecha: {Fecha} - Tipo: {Tipo} - Tipo de Tarjeta: {TipoTarjeta} - Línea: {Linea} - Monto: ${Monto} - Saldo restante: ${SaldoRestante} - ID Tarjeta: {TarjetaId}");
+
+            if (SaldoNegativoCancelado)
+            {
+                Console.WriteLine($"El saldo negativo ha sido cancelado con este pago.");
+            }
         }
     }
 }
